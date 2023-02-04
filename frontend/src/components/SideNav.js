55207d5a64ce1f8ddd01bc1e57a4f51525b0bb15
Rei Kong <reiikongs@gmail.com>
2023-02-04 17:52:41 -0500

    fixing

---
+++ frontend/src/components/SideNav.js
@@ -0,0 +1,36 @@
+import './SideNav.css';
+import React, { useState } from "react";
+
+
+function SideNav() {
+    return (
+        <div className='SideNav'>
+            <nav>
+                <ul>
+    
+                    <li>
+                        <p>meow</p>
+                    </li>
+    
+    
+                    <li>
+                        <p>meow</p>
+                    </li>
+                    <li>
+                        <p>meow</p>
+                    </li>
+                    <li>
+                        <p>meow</p>
+                    </li>
+    
+    
+                </ul>
+            </nav>
+    
+        </div>
+      )
+    
+}
+  
+
+export default SideNav
