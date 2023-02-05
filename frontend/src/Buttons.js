7a748ae16581ca6d12fd90ac9642d8a7f40df626
Rei Kong <reiikongs@gmail.com>
2023-02-05 00:25:38 -0500

    styling nav bar and buttons

--- frontend/src/Buttons.js
+++ frontend/src/Buttons.js
@@ -1,7 +1,12 @@
 import * as React from 'react';
+import Button from '@mui/material/Button'; 
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
 
 export default function currLocationButton() {
-    return (
-        <Button variant = "contained" endIcon =
-    )
+   return (
+       <Button variant = "contained" startIcon = {<LocationOnIcon />}>
+           Current Location
+       </Button>
+   );
 }
--- frontend/src/components/SideNav.js
+++ frontend/src/components/SideNav.js
@@ -1,19 +1,113 @@
 import './SideNav.css';
 import React, { useState } from "react";
+import oneCardLogoGray from './images/oneCardLogoGray.png';
 
 
 function SideNav() {
     return (
         <div className='side-nav'>
-            <div>
-                <div>
-                    meow
+            <div id="wrapper">
+                <div className='place-overview'>
+                    <p className='place-name'>Basha Sherbrooke</p>
+                    <p>
+                        <span>★★★</span>
+                        <span>★★</span>
+                    </p>
+                    <p>
+                        <span>Lebanese restaurant</span>
+                        <span> · </span>
+                        <span>660 Milton St</span>
+                    </p>
+                    <p>
+                        <span>Open</span>
+                        <span> · </span>
+                        <span>Closes 11 p.m.</span>
+                    </p>
+                    <p>
+                        <span>$</span>
+                        <span> · </span>
+                        <img style={{ height: 22 }} src={oneCardLogoGray}></img> 
+                    </p>
                 </div>
-                <div>
-                    meow
+
+                <hr className='divider' />
+            
+                <div className='place-overview'>
+                        <p className='place-name'>Basha Sherbrooke</p>
+                        <p>
+                            <span>★★★</span>
+                            <span>★★</span>
+                        </p>
+                        <p>
+                            <span>Lebanese restaurant</span>
+                            <span> · </span>
+                            <span>660 Milton St</span>
+                        </p>
+                        <p>
+                            <span>Open</span>
+                            <span> · </span>
+                            <span>Closes 11 p.m.</span>
+                        </p>
+                        <p>
+                            <span>$</span>
+                            <span> · </span>
+                            <img style={{ height: 22 }} src={oneCardLogoGray}></img> 
+                        </p>
                 </div>
+
+                <hr className='divider' />
+            
+                <div className='place-overview'>
+                        <p className='place-name'>Basha Sherbrooke</p>
+                        <p>
+                            <span>★★★</span>
+                            <span>★★</span>
+                        </p>
+                        <p>
+                            <span>Lebanese restaurant</span>
+                            <span> · </span>
+                            <span>660 Milton St</span>
+                        </p>
+                        <p>
+                            <span>Open</span>
+                            <span> · </span>
+                            <span>Closes 11 p.m.</span>
+                        </p>
+                        <p>
+                            <span>$</span>
+                            <span> · </span>
+                            <img style={{ height: 22 }} src={oneCardLogoGray}></img> 
+                        </p>
+                </div>
+
+                <hr className='divider' />
+
+                <div className='place-overview'>
+                        <p className='place-name'>Basha Sherbrooke</p>
+                        <p>
+                            <span>★★★</span>
+                            <span>★★</span>
+                        </p>
+                        <p>
+                            <span>Lebanese restaurant</span>
+                            <span> · </span>
+                            <span>660 Milton St</span>
+                        </p>
+                        <p>
+                            <span>Open</span>
+                            <span> · </span>
+                            <span>Closes 11 p.m.</span>
+                        </p>
+                        <p>
+                            <span>$</span>
+                            <span> · </span>
+                            <img style={{ height: 22 }} src={oneCardLogoGray}></img> 
+                        </p>
+                </div>
+
+                <hr className='divider' />
+
             </div>
-    
         </div>
     );
 }
