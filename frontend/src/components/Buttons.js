bd845acb4dcd0a498bf22a39406d2dca30dd13dd
leticia1232 <leticia.huang@gmail.com>
2023-02-05 10:20:25 -0500

    changing buttons
    location button

--- frontend/src/components/Buttons.js
+++ frontend/src/components/Buttons.js
@@ -1,12 +1,43 @@
 import * as React from 'react';
-import Button from '@mui/material/Button'; 
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 
-export default function currLocationButton() {
-   return (
-       <Button variant = "contained" startIcon = {<LocationOnIcon />}>
-           Current Location
-       </Button>
-   );
+class Buttons extends React.Component {
+    constructor(props) {
+        super(props);
+        this.click = this.click.bind(this);
+        this.hasLocation = this.hasLocation.bind(this);
+        this.error = this.error.bind(this);
+    };
+
+    // ref: https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+    hasLocation(pos) {
+        const crd = pos.coords;
+
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+
+        console.log(`Calling backend...`);
+
+        // ref: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+        fetch('http://localhost/backend')
+            .then((response) => response.json())
+            .then((data) => console.log(data)); // Can use the "data"
+    }
+
+    error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    
+    click() {
+        console.log('Click happened');
+        navigator.geolocation.getCurrentPosition(this.hasLocation, this.error);
+    }
+
+    render() {
+        return <button type="button" onClick={this.click}>Send my location</button>;
+    }
 }
+
+export default Buttons;
--- frontend/src/components/SideNav.js
+++ frontend/src/components/SideNav.js
@@ -1,12 +1,18 @@
 import './SideNav.css';
 import React, { useState } from "react";
 import oneCardLogoGray from './images/oneCardLogoGray.png';
+import Button from './Buttons';
 
 
 function SideNav() {
     return (
         <div className='side-nav'>
             <div id="wrapper">
+                <div>
+                    <Buttons />
+                </div>
+                <hr className='divider' />
+
                 <div className='place-overview'>
                     <p className='place-name'>Basha Sherbrooke</p>
                     <p>
@@ -26,83 +32,83 @@ function SideNav() {
                     <p>
                         <span>$</span>
                         <span> · </span>
-                        <img style={{ height: 22 }} src={oneCardLogoGray}></img> 
+                        <img style={{ height: 22 }} src={oneCardLogoGray}></img>
                     </p>
                 </div>
 
                 <hr className='divider' />
-            
-                <div className='place-overview'>
-                        <p className='place-name'>Basha Sherbrooke</p>
-                        <p>
-                            <span>★★★</span>
-                            <span>★★</span>
-                        </p>
-                        <p>
-                            <span>Lebanese restaurant</span>
-                            <span> · </span>
-                            <span>660 Milton St</span>
-                        </p>
-                        <p>
-                            <span>Open</span>
-                            <span> · </span>
-                            <span>Closes 11 p.m.</span>
-                        </p>
-                        <p>
-                            <span>$</span>
-                            <span> · </span>
-                            <img style={{ height: 22 }} src={oneCardLogoGray}></img> 
-                        </p>
-                </div>
 
-                <hr className='divider' />
-            
                 <div className='place-overview'>
-                        <p className='place-name'>Basha Sherbrooke</p>
-                        <p>
-                            <span>★★★</span>
-                            <span>★★</span>
-                        </p>
-                        <p>
-                            <span>Lebanese restaurant</span>
-                            <span> · </span>
-                            <span>660 Milton St</span>
-                        </p>
-                        <p>
-                            <span>Open</span>
-                            <span> · </span>
-                            <span>Closes 11 p.m.</span>
-                        </p>
-                        <p>
-                            <span>$</span>
-                            <span> · </span>
-                            <img style={{ height: 22 }} src={oneCardLogoGray}></img> 
-                        </p>
+                    <p className='place-name'>Basha Sherbrooke</p>
+                    <p>
+                        <span>★★★</span>
+                        <span>★★</span>
+                    </p>
+                    <p>
+                        <span>Lebanese restaurant</span>
+                        <span> · </span>
+                        <span>660 Milton St</span>
+                    </p>
+                    <p>
+                        <span>Open</span>
+                        <span> · </span>
+                        <span>Closes 11 p.m.</span>
+                    </p>
+                    <p>
+                        <span>$</span>
+                        <span> · </span>
+                        <img style={{ height: 22 }} src={oneCardLogoGray}></img>
+                    </p>
                 </div>
 
                 <hr className='divider' />
 
                 <div className='place-overview'>
-                        <p className='place-name'>Basha Sherbrooke</p>
-                        <p>
-                            <span>★★★</span>
-                            <span>★★</span>
-                        </p>
-                        <p>
-                            <span>Lebanese restaurant</span>
-                            <span> · </span>
-                            <span>660 Milton St</span>
-                        </p>
-                        <p>
-                            <span>Open</span>
-                            <span> · </span>
-                            <span>Closes 11 p.m.</span>
-                        </p>
-                        <p>
-                            <span>$</span>
-                            <span> · </span>
-                            <img style={{ height: 22 }} src={oneCardLogoGray}></img> 
-                        </p>
+                    <p className='place-name'>Basha Sherbrooke</p>
+                    <p>
+                        <span>★★★</span>
+                        <span>★★</span>
+                    </p>
+                    <p>
+                        <span>Lebanese restaurant</span>
+                        <span> · </span>
+                        <span>660 Milton St</span>
+                    </p>
+                    <p>
+                        <span>Open</span>
+                        <span> · </span>
+                        <span>Closes 11 p.m.</span>
+                    </p>
+                    <p>
+                        <span>$</span>
+                        <span> · </span>
+                        <img style={{ height: 22 }} src={oneCardLogoGray}></img>
+                    </p>
+                </div>
+
+                <hr className='divider' />
+
+                <div className='place-overview'>
+                    <p className='place-name'>Basha Sherbrooke</p>
+                    <p>
+                        <span>★★★</span>
+                        <span>★★</span>
+                    </p>
+                    <p>
+                        <span>Lebanese restaurant</span>
+                        <span> · </span>
+                        <span>660 Milton St</span>
+                    </p>
+                    <p>
+                        <span>Open</span>
+                        <span> · </span>
+                        <span>Closes 11 p.m.</span>
+                    </p>
+                    <p>
+                        <span>$</span>
+                        <span> · </span>
+                        <img style={{ height: 22 }} src={oneCardLogoGray}></img>
+                    </p>
                 </div>
 
                 <hr className='divider' />
@@ -111,6 +117,6 @@ function SideNav() {
         </div>
     );
 }
-  
+
 
 export default SideNav;
